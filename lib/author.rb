0293b7d41e3e23061399208e89a86c9bf4ebb87e
Blakeley Bond <burrick2003@yahoo.com>
2017-11-12 18:24:55 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,6 @@
 class Author
 attr_accessor :name
-@@posts = 0
+@@post_count = 0
 
   def initialize(name_input)
     self.name = name_input
@@ -12,20 +12,20 @@ attr_accessor :name
   end
 
   def self.post_count #forgot class method
-    @@posts
+    @@post_count
   end
 
   def add_post(post) #rspec sends in a post instance, going to link it with Author properties
     self.posts << post
     post.author = self
-    @@posts += 1
+    @@post_count += 1
   end
 
   def add_post_by_title(post_to_be)#like above but generates the post instance, not respectively
     post = Post.new(post_to_be)
     @posts << post
     post.author = self
-    @@posts+=1
+    @@post_count+=1
   end
 
 end
