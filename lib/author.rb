c7887c5725c137053ad421705d44e2b2307d5762
Blakeley Bond <burrick2003@yahoo.com>
2017-11-12 18:40:56 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -17,13 +17,12 @@ attr_accessor :name
 
   def add_post(post_instance) #rspec sends in an EXISTING post instance, going to link it with Author properties
     @posts << post_instance
-    post_instance.author = self #the magic line, that post over there is now here
+    post_instance.author = self #the magic line, that post over there is now here.  The author over there is now this instance of author.  The parent class claims ownership of the subordinate.
     @@post_count += 1
   end
 
   def add_post_by_title(post_to_be)#like above but generates the post instance, not respectively
     post = Post.new(post_to_be) #that post over there
-
     post.author = self #is here
     @posts << post
     @@post_count+=1
