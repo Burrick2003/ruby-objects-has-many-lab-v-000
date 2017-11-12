159ba8113df89b66c31ea28158261ec0fec3f403
Blakeley Bond <burrick2003@yahoo.com>
2017-11-12 17:59:26 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -4,7 +4,7 @@ attr_accessor :author, :title
   def initialize(author_name)
     @author = author_name
   end
-  def title
+  def author_name #called directly by spec, was using title
     if self.author
       self.author.name
     else
