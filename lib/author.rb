d317555bfc89b882ba3fd68dc7dcc8a74781c081
Blakeley Bond <burrick2003@yahoo.com>
2017-11-12 18:31:05 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -17,14 +17,14 @@ attr_accessor :name
 
   def add_post(post_instance) #rspec sends in an EXISTING post instance, going to link it with Author properties
     @posts << post_instance
-    post_instance.author = self
+    post_instance.author = self #the magic line, that post over there is now here
     @@post_count += 1
   end
 
   def add_post_by_title(post_to_be)#like above but generates the post instance, not respectively
-    post = Post.new(post_to_be)
+    post = Post.new(post_to_be) #that post over there
     @posts << post
-    post.author = self
+    post.author = self #is here
     @@post_count+=1
   end
 
