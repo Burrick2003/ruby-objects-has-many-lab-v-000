781f0cfc99d7fcf57963e7762a06aef01389bd1c
Blakeley Bond <burrick2003@yahoo.com>
2017-11-12 18:39:51 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -4,9 +4,9 @@ attr_accessor :author, :title
   def initialize(post_title)
     @title = post_title
   end
-  def author_name #called directly by spec, was using title
-    if self.author
-      self.author.name
+  def author_name #called directly by spec, was using author
+    if self.author #does this instance have the added, hybridized Author attributes?
+      self.author.name #doesn't own this, requests the author's name from the parent class (or rather retrieves from its own hybrid properties)
     else
       nil
     end
